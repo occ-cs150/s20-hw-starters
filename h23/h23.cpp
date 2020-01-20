26b760efbfc311ce59be60e1af69e459176f4d5c
Stephen Gilbert <sgilbert@student.cccd.edu>
2020-01-20 20:37:26 +0000

    Update h23 starters

--- h23/h23.cpp
+++ h23/h23.cpp
@@ -1,46 +1,18 @@
 /**
  *  @author Put your name here
  *  @date Put the date here
- *  @file h14.cpp
+ *  @file h23.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "sgilbert"; //"WHO AM I?"; // Add your Canvas/occ-email ID
-
+string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 
 #include "h23.h"
 
 // Add your code here
-int alternatingSum(const int a[], size_t size)
-{
-    int sum = 0;
-    for (size_t i = 0; i < size; i++)
-        if (i % 2 == 0)
-            sum += a[i];
-        else
-            sum -= a[i];
 
-    return sum;
-}
-
-
-MinMax minMax(const double *ptr, size_t size)
-{
-    MinMax result;
-    if (size > 0)
-    {
-        const double *aEnd = ptr + size;
-        result.min = result.max = ptr;
-        while (++ptr != aEnd)
-        {
-            if (*ptr > *result.max) result.max = ptr;
-            if (*ptr < *result.min) result.min = ptr;
-        }
-    }
-    return result;
-}
 
 
 /////////////// STUDENT TESTING ////////////////////
