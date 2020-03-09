ac6a987d030e02f33d4e839e923cbfc5484735fc
Stephen Gilbert <sgilbert@student.cccd.edu>
2020-03-09 20:36:14 +0000

    auto commit 03/09/20 20:36:13

--- h13/h13.cpp
+++ h13/h13.cpp
@@ -15,7 +15,32 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 #include "h13.h"
 
 // Implement your function here
+string dataSets(const string& fname)
+{
+    string result;
+    ifstream in(fname);
+    if (in.fail()) { return fname + " cannot be found."; }
 
+    // 3 4.5 2 9.8 7 4.5 0 4 2.7 5 3.9 0 2 3.2 4 7.1 0
+    int dataset = 1;
+    int repeat;
+    in >> repeat;
+    while (! in.fail())
+    {
+        double sum = 0.0;
+        int count = 0;
+        while (repeat != 0)
+        {
+            double value;
+            in >> value;
+            count += repeat;
+            sum += repeat * value;
+        }
+
+    }
+
+    return result;
+}
 
 
 
